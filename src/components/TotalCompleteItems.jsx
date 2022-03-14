5a94176c1d31a3f410181a17e75ba2d6e6792748
Enes İnan <enes.inan@vbt.com.tr>
2022-03-14 22:20:21 +0300

    Component contains completed total items

---
+++ src/components/TotalCompleteItems.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+
+const TotalCompleteItems = () => {
+	const completedTodos = useSelector( (state) => state.todos.filter((todo) => todo.completed === true));
+  
+	return <h4 className='mt-3 total-text'>Total Complete Items: {completedTodos.length}</h4>;
+};
+
+export default TotalCompleteItems;
