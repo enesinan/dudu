09d4febb52ba71d0b640015d2dcfd0d93b5d8700
Enes İnan <enes.inan@vbt.com.tr>
2022-03-14 22:19:41 +0300

    Todolist component added

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+import { useSelector } from 'react-redux';
+
+const TodoList = () => {
+	const todos = useSelector((state) =>state.todos);
+
+	return (
+		<ul className='list-group'>
+			{todos.map((todo) => (
+				<TodoItem id={todo.id} title={todo.title} completed={todo.completed} />
+			))}
+		</ul>
+	);
+};
+
+export default TodoList;
